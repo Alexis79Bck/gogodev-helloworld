b718f8417083abe83515fd035969c481e5c2bad6
Alexis E. Mata <alexedmata@gmail.com>
2024-11-25 15:30:57 -0400

    Creado Plantilla Base Landing

--- resources/views/landing/contact.blade.php
+++ resources/views/landing/contact.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Us | GOGODEV - Laravel</title>
-</head>
-<body>
+@extends('layouts.landing')
+
+@section('title', 'Contact Us')
+
+@section('content')
     <div>
         <h1>CONTACT US</h1>
     </div>
-</body>
-</html>
+@endsection
