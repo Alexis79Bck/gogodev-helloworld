605588e2cf78ec2a75cb80e5c6a4208b33d2a549
Alexis E. Mata <alexedmata@gmail.com>
2024-11-26 23:59:04 -0400

    agregado style.css y algunas imagenes

--- resources/views/components/landing/service/card.blade.php
+++ resources/views/components/landing/service/card.blade.php
@@ -2,5 +2,8 @@
 
 <div style="border: 2px solid blue; border-radius: 10px; padding: 10px; margin: 12px">
     <h3>{{ $title }}</h3>
+    <div>
+        <img src="{{ asset('assets/images/' . $photo ) }}" alt="photo 1" />
+    </div>
     <p>{{ $bodyContent }}</p>
 </div>
--- resources/views/landing/services.blade.php
+++ resources/views/landing/services.blade.php
@@ -10,6 +10,8 @@
     {{-- <x-landing.service.card title="Service 1" bodyContent="lorem Ipsum Mae" /> --}}
     @component('components.landing.service.card')
         @slot('title', 'Service 1')
+
+        @slot('photo', 'photo-1.jpg')
         
         @slot('bodyContent')
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut nobis esse assumenda, quaerat ipsam quasi. Alias accusamus libero ipsum suscipit, illum odit quibusdam? Minima obcaecati adipisci sit numquam dolorem maxime.
@@ -18,6 +20,8 @@
 
     @component('components.landing.service.card')
         @slot('title', 'Service 2')
+
+        @slot('photo', 'photo-2.jpg')
         
         @slot('bodyContent')
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut nobis esse assumenda, quaerat ipsam quasi. Alias accusamus libero ipsum suscipit, illum odit quibusdam? Minima obcaecati adipisci sit numquam dolorem maxime.
@@ -26,6 +30,8 @@
 
     @component('components.landing.service.card')
         @slot('title', 'Service 3')
+
+        @slot('photo', 'photo-3.jpg')
         
         @slot('bodyContent')
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut nobis esse assumenda, quaerat ipsam quasi. Alias accusamus libero ipsum suscipit, illum odit quibusdam? Minima obcaecati adipisci sit numquam dolorem maxime.
