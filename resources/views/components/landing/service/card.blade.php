0815ec5440aa132c7e1a7e89f9ab5275e6254471
Alexis E. Mata <alexedmata@gmail.com>
2024-11-26 21:03:12 -0400

    creado component service card

---
+++ resources/views/components/landing/service/card.blade.php
@@ -0,0 +1,6 @@
+{{-- @props(['title', 'bodyContent']) --}}
+
+<div style="border: 2px solid blue; border-radius: 10px; padding: 10px; margin: 12px">
+    <h3>{{ $title }}</h3>
+    <p>{{ $bodyContent }}</p>
+</div>
--- resources/views/landing/services.blade.php
+++ resources/views/landing/services.blade.php
@@ -6,4 +6,29 @@
     <div>
         <h1>SERVICES</h1>
     </div>
+    <hr height='3px'  />
+    {{-- <x-landing.service.card title="Service 1" bodyContent="lorem Ipsum Mae" /> --}}
+    @component('components.landing.service.card')
+        @slot('title', 'Service 1')
+        
+        @slot('bodyContent')
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut nobis esse assumenda, quaerat ipsam quasi. Alias accusamus libero ipsum suscipit, illum odit quibusdam? Minima obcaecati adipisci sit numquam dolorem maxime.
+        @endslot
+    @endcomponent
+
+    @component('components.landing.service.card')
+        @slot('title', 'Service 2')
+        
+        @slot('bodyContent')
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut nobis esse assumenda, quaerat ipsam quasi. Alias accusamus libero ipsum suscipit, illum odit quibusdam? Minima obcaecati adipisci sit numquam dolorem maxime.
+        @endslot
+    @endcomponent
+
+    @component('components.landing.service.card')
+        @slot('title', 'Service 3')
+        
+        @slot('bodyContent')
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut nobis esse assumenda, quaerat ipsam quasi. Alias accusamus libero ipsum suscipit, illum odit quibusdam? Minima obcaecati adipisci sit numquam dolorem maxime.
+        @endslot
+    @endcomponent
 @endsection
