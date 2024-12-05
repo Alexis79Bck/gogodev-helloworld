02dc829d0ead5c272239df3ce0c3b716e4f1e3db
Alexis E. Mata <alexedmata@gmail.com>
2024-12-05 13:59:29 -0400

    Creada las rutas crud para Modelo Note

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +13,12 @@ Route::view('/contact-us', 'landing.contact')->name('landing.contact');
 
 Route::get('/testing/users', [UserController::class, 'index'])->name('user.index');
 Route::get('/testing/new-user', [UserController::class, 'create'])->name('user.create');
+
+Route::get('/testing/notes', [NoteController::class, 'index'])->name('notes.index');
+Route::get('/testing/new-note', [NoteController::class, 'create'])->name('note.create');
+Route::post('/testing/new-note', [NoteController::class, 'store'])->name('note.store');
+Route::get('/testing/note/{id}', [NoteController::class, 'show'])->name('note.show');
+Route::get('/testing/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+Route::put('/testing/note/{id}', [NoteController::class, 'update'])->name('note.update');
+Route::delete('/testing/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+
